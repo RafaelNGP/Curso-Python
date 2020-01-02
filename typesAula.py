6ab474fd435f265add8e528932304ee08587a213
RafaelNGP <rafaelferreira.dev3@gmail.com>
2020-01-02 00:20:04 -0300

    Arquivo renomeado
    O arquivo mudou de types para typesAula para nao dar conflito com o arquivo existente com o mesmo nome.

--- typesAula.py
+++ typesAula.py
@@ -1,15 +1,16 @@
-x = 7 # INT
+# INT
+x = 7
 print(f'x is {x}')
 print(type(x))
-
-y = True #BOOLEAN
+# BOOLEAN
+y = True
 print(f'y is {y}')
 print(type(y))
-
-z = "String" # '' e "" são iguais
+# '' e "" são iguais
+z = "String"
 print(f'Z is {z}')
 print(type(z))
-
-X = 7.0 #FLOAT
+#FLOAT
+X = 7.0
 print(f'X.0 is {X}')
 print(type(X))
