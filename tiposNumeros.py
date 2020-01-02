329c359e04abb2cdb21edd070d09d3639f475b97
RafaelNGP <rafaelferreira.dev3@gmail.com>
2020-01-02 11:43:53 -0300

    Criacao do arquivo e desenvolvimento do conceito
    Usando console para realizar expressoes matematicas e guardei os resultados como comentario.
    criando e definindo o valor de uma variavel e printando ela uma vez, alem de usar __add__ para testar.

---
+++ tiposNumeros.py
@@ -0,0 +1,20 @@
+"""
+# Tipo Numerico
+#
+# >>> num = 42
+# >>> num
+# 42
+# >>> num += 2
+# >>> num
+# 44
+# >>> num *= 2
+# >>> num
+# 88
+# >>> type(num)
+# <class 'int'>
+"""
+
+num = 1000
+print(num)
+
+print(num.__add__(8))
