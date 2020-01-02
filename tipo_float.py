cffec1ba94f10784532b7846e25f871c75fc91c8
RafaelNGP <rafaelferreira.dev3@gmail.com>
2020-01-02 11:57:18 -0300

    Criacao do arquivo e desenvolvimento do conceito
    Usando console para realizar expressoes matematicas e guardei os resultados como comentario.

---
+++ tipo_float.py
@@ -0,0 +1,35 @@
+"""
+Tipo Float
+
+Tipo real, decimal
+
+casas decimais
+
+OBS? o separador de casas decimais eh ponto e nao virtula.
+"""
+# Criando float
+valor = 1.44
+print(valor)
+print(type(valor))
+
+# Cria uma tupla em vez de float
+valor = 1, 44
+print(valor)
+print(type(valor))
+
+# Evitar dupla atribuicao
+valor1,  valor2 = 1, 44
+print(valor1)
+print(valor2)
+
+# Convertendo valores
+res = int(valor)
+print(valor)
+print(type(valor))
+
+# Podemos trabalhar numeros complexos
+# >>> var = 5j
+# >>> type(var)
+# <class 'complex'>
+# >>> var ** 2
+# (-25+0j)
