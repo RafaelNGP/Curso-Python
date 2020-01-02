fed4febf530eed2ef9f791820bff2a54879a2c45
RafaelNGP <rafaelferreira.dev3@gmail.com>
2020-01-02 00:18:07 -0300

    Demonstrando dir e help
    Testando direto no console e apenas uma chamada no codigo, para ver como fazer.

---
+++ dir_e_help.py
@@ -0,0 +1,9 @@
+"""
+Utilitarios Python para auxiliar na programacao
+
+dir -> Apresenta todos os atributos/propriedades e funcoes/metodos disponiveis para determinado tipo de dado ou variavel.
+
+help -> Apresenta a documentacao/como utilizar os atributos/propriedades e funcoes/metodos disponiveis para determinado
+tipo de dado ou variavel.
+"""
+print(dir('geek'))
