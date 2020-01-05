23f7e4390525151e8a15f93aad9e95c1a1caf544
RafaelNGP <rafaelferreira.dev3@gmail.com>
2020-01-04 21:16:28 -0300

    Commit inicial da aula sobre repeticao for
    Inclui um exemplo que pensei fora da aula, usando print([]) que ainda n foi abordado.

---
+++ loop_for.py
@@ -0,0 +1,50 @@
+"""
+Loop For
+
+C ou Java
+for (int i = 0; i < limitador; i++){
+    //codigo
+
+Python
+
+For item in interavel:
+    //codigo
+
+utilizamos loops para iterar sobre sequencias ou sobre valores iteraveis
+-Strings
+-Listas
+-Range
+"""
+
+nome = "Geek University"
+lista = [1, 2, 3, 4, 5]
+numeros = range(1,10)
+
+for letra in nome:
+    print(letra)
+
+for numero in lista:
+    print(numero)
+
+for numero in range(1, 10):
+    print(numero)
+
+for valor in enumerate(nome):
+    print(valor)
+
+print("A regra do jogo eh a seguinte: Cada vez que repetir, sera somado o numero de vezes repetidas!")
+soma = 0
+qnt = int(input("Quantas vezes o loop deveria rodar? "))
+
+for vezes in range(1, qnt+1):
+    soma += vezes
+    print([vezes, soma])
+
+soma = 0
+for vezes in range(1, qnt+1):
+    soma += vezes
+    print(f"Somando pela {vezes} vez, o total eh {soma}")
+
+for _ in range(3):
+    for num in range(1, 11):
+        print('\U0001F60D' * num)
