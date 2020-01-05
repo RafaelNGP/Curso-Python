685add20d795cd6bf70e3b2375c91b32ee214e6c
RafaelNGP <rafaelferreira.dev3@gmail.com>
2020-01-04 22:22:49 -0300

    Adicionado exemplo break e cada resultado em uma nova linha.

--- loop_while.py
+++ loop_while.py
@@ -15,8 +15,17 @@ while numero <= 10:
     print(numero, end=' ')
     numero += 1
 
-# Em um loop while eh importante que cuidemos do criterio de parada - (Loop infinito)
+# Ex 2
+numero = 0
+print()
+while numero <= 10:
+    if numero == 6:
+        break
+    print(numero, end=' ')
+    numero += 1
 
+# Em um loop while eh importante que cuidemos do criterio de parada - (Loop infinito)
+print()
 resposta = ''
 while resposta != "s":
     resposta = input("Ja acabou Jessica? ")
