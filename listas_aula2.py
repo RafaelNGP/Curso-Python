37ced94b37b0bdca11e2230f7837dcd0e531b4ff
RafaelNGP <rafaelferreira.dev3@gmail.com>
2020-01-05 16:37:13 -0300

    Commit da segunda parte da aula sobre listas
    Novo arquivo criado para nao misturar exemplos

---
+++ listas_aula2.py
@@ -0,0 +1,21 @@
+"""
+Listas
+
+Funcionam como vetores/matrizes (arrays) com a diferenca de serem dinamicos
+e tambem podermos colocar qualquer tipo de dado.
+
+C ou Java: Arrays
+    - Possuem tamanho e tipo de dado fixo;
+
+Python
+- Dinamico: Nao possui tamanho fixo; ou seja, podemos criar a lista e ir adicionando elementos.
+- Qualquer tipo de dado: Nao possuem tipagem de dado, entao qualquer dado pode ser adicionado.
+
+As listas em Python sao representadas por colchetes []
+"""
+
+lista1 = [1, 99, 4, 27, 15, 22, 3, 1, 44, 42, 27]
+lista2 = ["G", "E", "E", "K", "", "U", "N", "I", "V", "E", "R", "S", "I", "T", "Y"]
+lista3 = []
+lista4 = list(range(11))
+lista5 = list("Geek University")
