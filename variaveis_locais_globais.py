f69b6edd7b97c0b81a0da77676f12476479f1578
RafaelNGP <rafaelferreira.dev3@gmail.com>
2020-01-09 23:27:21 -0300

    Movendo a definicao das funcoes, adicionado um novo exemplo
    Exemplo usando a palavra reservada 'global' para utilizar a variavel que se encontra fora do escopo e tambem uma nova funcao para fazer o teste desse recurso.

--- variaveis_locais_globais.py
+++ variaveis_locais_globais.py
@@ -3,8 +3,6 @@ Variaveis locais e globais
 
 Inicialmente esta nao era uma aula separada, mas criei este arquivo para testar.
 """
-instrutor = "Geek - Global"
-print(instrutor)
 
 
 def diz_oi():
@@ -12,5 +10,16 @@ def diz_oi():
     return print(instrutor)
 
 
+def diz_oiLocal():
+    global instrutor
+    # Ao invocar o global, ele esta mudando o valor da variavel global dessa forma e nao da local.
+    instrutor = "Geek" + " - Local"
+    return print(instrutor)
+
+
+instrutor = "Geek - Global"
+print(instrutor)
+
 diz_oi()
+diz_oiLocal()
 print(instrutor)
