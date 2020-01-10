bdb7df4b7bd9344a7a20cad6df2370f3fe11716b
RafaelNGP <rafaelferreira.dev3@gmail.com>
2020-01-10 00:34:56 -0300

    Utilizando a funcao help e pripriedade __doc__

--- docstrings_aula.py
+++ docstrings_aula.py
@@ -1,3 +1,20 @@
 """
 Documentando funcoes com docstrings
+
+OBS: Podemos ter acesso a documentacao de uma funcao em python utilizando
+a propriedade especial __doc__
+
+Podemos ainda fazer acesso a documentacao com a funcao help()
 """
+
+
+def diz_oi():
+    """Uma funcao simples que retorna uma string com o conteudo 'Oi'"""
+    return 'Oi'
+
+
+# O conteudo que eh mostrado nesta chamada eh o docstring da funcao.
+# print(help(print()))
+print(diz_oi())
+print(help(diz_oi))
+print(diz_oi.__doc__)
