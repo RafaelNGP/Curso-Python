0d8ab8480a4ae3d1ce0549742a0d16461dcdbc3d
RafaelNGP <rafaelferreira.dev3@gmail.com>
2020-01-20 20:52:06 -0300

    Desenvolvimento do assunto e exemplos

--- set_comprehension_aula.py
+++ set_comprehension_aula.py
@@ -1,5 +1,21 @@
 """
 Set comprehension
 
-
+lista = [1, 2, 3, 4, 5]
+set = {1, 2, 3, 4, 5}
 """
+# Exemplos
+numeros = {num for num in range(1, 7)}
+print(numeros)
+
+# outro exemplo
+numeros = {x ** 2 for x in range(10)}
+print(numeros)
+
+# Faca uma alteracao na estrutura acima para gerar um dicionario ao inves de um set
+numeros = {x: x ** 2 for x in range(10)}
+print(numeros)
+
+# Finalizando com mais um exemplo
+letras = {letra for letra in 'Geek University'}
+print(letras)
