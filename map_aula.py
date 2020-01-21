22ade2925ba45e8b1d367d548583a80b4e4b0b09
RafaelNGP <rafaelferreira.dev3@gmail.com>
2020-01-21 00:12:58 -0300

    Complementando com exemplos novos da aula e um proprio.

--- map_aula.py
+++ map_aula.py
@@ -29,5 +29,30 @@ print(list(areas))
 
 # Exemplo proprio
 print(list(map(lambda r: math.pi * (r ** 2), raios)))
-
 # Apos utilizar a funcao map() depois da primeira utilizacao do resultado, ele zera.
+
+"""
+Para fixar - Map
+
+temos dados iteraveis:
+    dados: a1, a2 ... an
+    
+temos uma funcao:
+    funcao: f(x)
+    
+utilizamos a funcao map(f, dados) onde map ira mapear cada elemento dos dados
+e aplicar a funcao.
+    O map object: f(a1), f(a2), f(...), f(an)
+"""
+
+cidades = [('Berlin', 29), ('Cairo', 36), ('Buenos Aires', 19), ('Los Angeles', 26), ("Tokyo", 27)]
+print(cidades)
+# Formula para converter c para f
+# f = 9/5 * c + 32
+
+# lambda
+c_para_f = lambda dado: (dado[0], (9/5) * dado[1] + 32)
+print(list(map(c_para_f, cidades)))
+
+# exemplo proprio - metodo direto
+print(list(map(lambda dado: (dado[0], (9/5) * dado[1] + 32), cidades)))
