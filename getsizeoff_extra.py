de03cbccfbbfe98aa567888b9b4ea7e49fb892e9
RafaelNGP <rafaelferreira.dev3@gmail.com>
2020-01-24 18:30:09 -0300

    iterando sobre generator com for simples

--- getsizeoff_extra.py
+++ getsizeoff_extra.py
@@ -10,11 +10,15 @@ from sys import getsizeof
 list_comp = getsizeof([x * 10 for x in range(1000)])
 set_comp = getsizeof({x * 10 for x in range(1000)})
 dict_comp = getsizeof({x: x * 10 for x in range(1000)})
-generator = getsizeof(x * 10 for x in range(1000))
+gen = getsizeof(x * 10 for x in range(1000))
 
 # Prints
 print(f'Tamanho de cada lista, em bytes:\n'
       f'Lista: {list_comp} bytes\n'
       f'Set: {set_comp} bytes\n'
       f'Dict: {dict_comp} bytes\n'
-      f'Generator: {generator} bytes')
+      f'Generator: {gen} bytes')
+
+gen = (x * 10 for x in range(1000))
+for num in gen:
+    print(num)
