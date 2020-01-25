42b5af6128ca5a4712080edd6a9b21151b952f36
RafaelNGP <rafaelferreira.dev3@gmail.com>
2020-01-24 23:26:37 -0300

    Desenvolvimento do conceito

--- reversed_aula.py
+++ reversed_aula.py
@@ -1,5 +1,26 @@
 """
 Reversed
 
-
+Nao confundir com a funcao reverse() das listas.
+A funcao do reversed eh inventer um iteravel, nao precisando ser apenas listas.
 """
+# Exemplos
+lista = [1, 2, 3, 4, 5]
+res = reversed(lista)
+print(res)
+print(list(res))
+print(type(res))
+
+tupla = 1, 2, 3, 4, 5
+res = reversed(tupla)
+print(res)
+print(tuple(res))
+print(type(res))
+
+# Usando reversed na iteracao
+for letra in reversed("Geek University"):
+    print(letra, end="")
+
+print()
+# Fazendo o mesmo sem for.
+print(''.join(list(reversed("Geek University"))))
