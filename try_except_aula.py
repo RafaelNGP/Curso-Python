36049116634cb31dc0a90d7986806c26b57c5817
RafaelNGP <rafaelferreira.dev3@gmail.com>
2020-01-28 22:31:13 -0300

    adicionados exemplo com else e teorias.

--- try_except_aula.py
+++ try_except_aula.py
@@ -1,5 +1,5 @@
 """
-Try/Except
+Try/Except/Else/Finally
 
 utilizamos para tratar erros que podem ocorrer no nosso codigo. Previnindo assim que o programa pare
 de funcionar e o usuario receba mensagens de erro inesperadas.
@@ -7,11 +7,15 @@ de funcionar e o usuario receba mensagens de erro inesperadas.
 A forma geral mais simples eh:
 
 try:
-    //execucao problematica
+    // execucao problematica
 except:
-    //O que deve ser feito em caso de problemas.
+    // O que deve ser feito em caso de problemas.
+else:
+    // Executado apenas se nao ocorrer o erro.
+finally:
+    // sempre eh executado, independente de ter tido um erro ou nao.
+Quando e onde tratar codigo: Toda entrada deve ser tratada.
 """
-
 # exemplo 1 - Tratando um erro generico
 try:
     geek()
@@ -23,3 +27,26 @@ except ValueError as err3:
     print(f"Nao recebe um valor esperado. Erro: {err3}")
 
 # Em resumo: Tente executar a funcao, caso encontre erros, imprima o outro bloco em vez disso.
+
+try:
+    num = int(input("informe um numero: "))
+except ValueError as erro1:
+    print('Valor incorreto')
+else:
+    print(f'voce digitou: {num}')
+
+# exemplo mais complexo
+
+
+def dividir(a, b):
+    try:
+        return int(a) / int(b)
+    except (ValueError, ZeroDivisionError) as err:
+        return f"Valor incorreto. Erro: {err}"
+
+
+num1 = input('Informe o primeiro numero: ')
+num2 = input("Informe o segundo numero: ")
+
+print(dividir(num1, num2))
+
