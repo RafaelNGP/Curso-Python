844ceeda32a47c94920a7c55c8b6b50ca0892acc
RafaelNGP <rafaelferreira.dev3@gmail.com>
2020-02-07 15:45:56 -0300

    Commit inicial e desenvolvimento do tema
    O console esta bem baguncado por causa dos modulos escolhidos, gostaria de resolver isso criando modulos melhores.

---
+++ modulos_customizados.py
@@ -0,0 +1,17 @@
+"""
+Modulos Customizados - Udemy
+
+Como modulos Python nada mais sao do que arquivos python, entao TODOS os arquivos que criamos neste
+curso sao modulos Python prontos para serem utilizados.
+
+Um modulo Python nao deve conter execucao de codigo (como foi desenvolvido ate agora no curso, boa)
+entao o resultado no console desta aula mostra a execucao do codigo importado primeiro e apenas na ultima
+linha, que traz o resultado 10, que faz a resolucao da funcao importada soma(5,5).
+
+Se tivesse alguma variavel na importacao, tambem poderia usa-la aqui.
+"""
+import funcoes_parametro_default as fpd
+import listas_aula as lst
+
+print(fpd.soma(5, 5))
+print(lst.lista1, lst.lista2, lst.lista3, lst.lista4, lst.lista5, lst.num)
