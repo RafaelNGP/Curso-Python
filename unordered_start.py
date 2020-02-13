336477f3dda4e1c65a5d2bdec202d77035755edc
RafaelNGP <rafaelferreira.dev3@gmail.com>
2020-02-13 00:59:16 -0300

    Commit inicial -LinkedIn Learning

---
+++ unordered_start.py
@@ -0,0 +1,17 @@
+# searching for an item in an unordered list
+# sometimes called a Linear search
+
+# declare a list of values to operate on
+items = [6, 20, 8, 19, 56, 23, 87, 41, 49, 53]
+
+
+def find_item(item, item_list):
+    for i in range(len(items)):
+        if item == item_list[i]:
+            return i
+
+    return None
+
+
+print(find_item(87, items))
+print(find_item(250, items))
