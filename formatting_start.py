8f4f1489525867f5e4dd13ee52d6cec3c7e8d27d
RafaelNGP <rafaelferreira.dev3@gmail.com>
2020-02-18 15:34:38 -0300

    Commit inicial -LinkedIn Learning

---
+++ formatting_start.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+
+def main():
+    now = datetime.now()
+    print(now.strftime("The current year is: %Y"))
+    print(now.strftime("%a, %d %B, %y"))
+
+    print(now.strftime("Local date and time: %c"))
+    print(now.strftime("Local date: %x"))
+    print(now.strftime("Local time: %X"))
+
+    print(now.strftime("Current time: %I:%M:%S %p"))
+    print(now.strftime("24-hour time: %H:%M %p"))
+
+
+if __name__ == '__main__':
+    main()
