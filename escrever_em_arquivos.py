20295586187a0ab1fb7ac801d3946a558a225ea8
RafaelNGP <rafaelferreira.dev3@gmail.com>
2020-02-28 21:09:08 -0300

    utilizando modo r+ -Udemy
    Alem de possibilitar a leitura, permite a escrita.

--- escrever_em_arquivos.py
+++ escrever_em_arquivos.py
@@ -7,10 +7,12 @@ Da mesma forma, se abrirmos um arquivo para escrita, nao podemos le-lo, somente 
 
 # exemplo de escrita - modo 'w' - write(escrita)
 
-with open('novo.txt', 'w') as arquivo:
+with open('novo.txt', 'r+') as arquivo:
     while True:
         fruta = input("Informe uma fruta ou digite 'sair': ")
         if fruta != 'sair':
             arquivo.write(fruta + '\n')
         else:
+            arquivo.seek(0)
+            print(arquivo.read())
             break
